d4576a943d09450e19bc3b56d88a8560b5d8e772
Anton <anton.annlov@gmail.com>
2021-09-07 10:53:38 +0200

    Add basic character controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
@@ -35,47 +39,87 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        var (dir, tilt) = GetMoveInput();
+
+        CalculateSpeed(tilt);
+
+        var angleRotated = CalculateRotation(dir);
+        
+        // Calculate this frame's translation vector
+        var translationVector = _direction * (_speed * Time.deltaTime);
+        
+        // Move character
+        _controller.Move(translationVector);
+        // Rotate character
+        transform.Rotate(Vector3.up, angleRotated);
+    }
+
+    /// <summary>
+    /// Gets input from vertical and horizontal axis.
+    /// </summary>
+    /// <returns>Input axis' direction and tilt value.</returns>
+    private (Vector3, float) GetMoveInput()
+    {
         var vert = Input.GetAxis("Vertical");
         var hori = Input.GetAxis("Horizontal");
-        var input = new Vector2(vert, hori);
-
+        var input = new Vector2(hori, vert);
+        
         var tiltFactor = Vector2.Distance(new Vector2(0.0f, 0.0f), input);
-        if (tiltFactor > 0.0f)
+        
+        var inputDir = new Vector3(input.x, 0.0f, input.y);
+        inputDir = inputDir.normalized;
+
+        return (inputDir, tiltFactor);
+    }
+
+    /// <summary>
+    /// Calculates and updates the current speed.
+    /// </summary>
+    /// <param name="tilt">The input tilt factor</param>
+    private void CalculateSpeed(float tilt)
+    {
+        if (tilt > 0.0f)
         {
-            _speed += _acceleration * tiltFactor * Time.deltaTime;
-            if (_speed > _maxSpeed)
+            _speed += _acceleration * tilt * Time.deltaTime;
+            if (_speed > _maxSpeed * tilt)
             {
-                _speed = _maxSpeed;
+                _speed = _maxSpeed * tilt;
             }
         }
         else
         {
             _speed = 0.0f;
         }
+    }
 
-
-        var inputDir = new Vector3(input.x, 0.0f, input.y);
-        inputDir = inputDir.normalized;
-        
-        var angle = Vector3.SignedAngle(_direction, inputDir, new Vector3(0.0f, 1.0f, 0.0f));
+    /// <summary>
+    /// Calculate and update our direction
+    /// </summary>
+    /// <param name="inputDir">The input direction</param>
+    /// <returns>The angle rotated</returns>
+    private float CalculateRotation(Vector3 inputDir)
+    {
+        // Get the angle between the character direction and input direction
+        var angle = Vector3.SignedAngle(_direction, inputDir, Vector3.up);
         var sign = 1;
         if (angle < 0)
         {
             sign = -1;
         }
+        
+        // Calculate amount to rotate this frame according to rotation speed set
         var angleToRotate = _rotationSpeed * Time.deltaTime;
         if (angleToRotate > Math.Abs(angle))
         {
             angleToRotate = angle;
         }
 
+        // Make sure we rotate the correct direction
         angleToRotate *= sign;
-        
+
+        // update direction
         _direction = Quaternion.Euler(0.0f, angleToRotate, 0.0f) * _direction;
 
-        var moveVec = _direction * (_speed * Time.deltaTime);
-
-        var temp = new Vector3(moveVec.x, 0.0f, moveVec.y);
-        _controller.Move(temp);
+        return angleToRotate;
     }
 }
