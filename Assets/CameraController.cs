2ecaaf21a1d0be87a1c9729a4ef054cbd6157c61
Cornelis <cornelistsjobeck@gmail.com>
2021-09-08 21:03:48 +0200

    Add player-camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;        //Public variable to store a reference to the player game object
+
+    [SerializeField] private Vector3 offset;          //Private variable to store the offset distance between the player and camera
+
+    [SerializeField] private float angle;            //Camera angle
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset = new Vector3(0, 5.9f, -3.76f);
+        angle = 51;
+        
+        transform.Rotate(Vector3.right, angle);
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate () 
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.SetPositionAndRotation(player.transform.position + offset, Quaternion.AngleAxis(angle, Vector3.right));
+    }
+}
